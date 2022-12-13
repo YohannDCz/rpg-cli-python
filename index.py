c0df8e69a68e35362dab790772f7830d7a573dc1
YohannDCz <36419948+YohannDCz@users.noreply.github.com>
2022-12-13 10:49:27 +0100

    Fichier original du prof

---
+++ index.py
@@ -0,0 +1,119 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/YohannDCz/Projet_RPG_python/blob/main/index.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "current_position = \"here\"\n",
+        "player_name = \"Bob\"\n",
+        "game_map = \"\""
+      ],
+      "metadata": {
+        "id": "wqsx0gwJrRlU"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "iJM0smITmIo8"
+      },
+      "outputs": [],
+      "source": [
+        "\n",
+        "def start_game():\n",
+        "  return\n",
+        "\n",
+        "def inventory():\n",
+        "  return \n"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "def Menu():\n",
+        "  # Afficher les options du menu\n",
+        "  # demander à l'utilisateur \n",
+        "  # en fonction du choix appeler une des 4 fonctions du dessous\n",
+        "\n",
+        "def start_game():\n",
+        "  # afficher le contexte et le début de l'histoire\n",
+        "  # ask_name()\n",
+        "  # move()\n",
+        "\n",
+        "def ask_name():\n",
+        "  # demander le nom\n",
+        "  return name\n",
+        "\n",
+        "def load_game():\n",
+        "  # charger des datas en mémoire\n",
+        "  start_game()\n",
+        "\n",
+        "def credits():\n",
+        "  # print afficher des infos sur la teams \n",
+        "  Menu()\n",
+        "\n",
+        "def exit():\n",
+        "  # quitter le jeu"
+      ],
+      "metadata": {
+        "id": "bGrdyff0nlwD"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "def move():\n",
+        "  # print les propositions de déplacement\n",
+        "  # recupérer le choix de l'utilisateur \n",
+        "  # En fonction du choix appeler une fonctionnalité\n",
+        "\n",
+        "def fight():\n",
+        "  # présentation du combat\n",
+        "  # print propositions d'actions du joueur\n",
+        "  # en fonction du choix du joueurs appeler la fonction qui correspond\n",
+        "  # action de l'ennemis\n",
+        "\n",
+        "\n",
+        "def find_object():\n",
+        "  return\n",
+        "\n",
+        "def event():\n",
+        "  return "
+      ],
+      "metadata": {
+        "id": "GRYkBDSznlp2"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
