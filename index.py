c29e82cc1fa297d21e0f1aa4dd3bc5fe1d594b54
YohannDCz <36419948+YohannDCz@users.noreply.github.com>
2022-12-13 10:52:34 +0100

    Projet initial du prof

---
+++ index.py
@@ -0,0 +1,52 @@
+current_position = "here"
+player_name = "Bob"
+game_map = ""
+
+def start_game():
+  return
+
+def inventory():
+  return 
+  
+  def Menu():
+  # Afficher les options du menu
+  # demander à l'utilisateur 
+  # en fonction du choix appeler une des 4 fonctions du dessous
+
+def start_game():
+  # afficher le contexte et le début de l'histoire
+  # ask_name()
+  # move()
+
+def ask_name():
+  # demander le nom
+  return name
+
+def load_game():
+  # charger des datas en mémoire
+  start_game()
+
+def credits():
+  # print afficher des infos sur la teams 
+  Menu()
+
+def exit():
+  # quitter le jeu
+
+def move():
+  # print les propositions de déplacement
+  # recupérer le choix de l'utilisateur 
+  # En fonction du choix appeler une fonctionnalité
+
+def fight():
+  # présentation du combat
+  # print propositions d'actions du joueur
+  # en fonction du choix du joueurs appeler la fonction qui correspond
+  # action de l'ennemis
+
+
+def find_object():
+  return
+
+def event():
+  return 
