c3e81cfa7a1285ee75a5edbca89c0fb41cebb209
Yohann DC <YohannDCz@gmail.com>
2022-12-14 16:46:30 +0100

    Inclus les tables de déplacement

--- index.py
+++ index.py
@@ -1,3 +1,44 @@
+grille_debut = [[" ", " ", " ", " ", " ", " ", " ", ";"],
+                ["☐", "☐", "☐", "☐", "☐", "☐", "☐", ";"],
+                [" ", " ", " ", " ", " ", " ", " ", ";"],
+                [" ", "○", "○", "○", "○", "○", " ", ";"],
+                [";", " ", "☐", "☐", "☐", " ", ";", " "],
+                [" ", ";", " ", " ", " ", ";", " ", " "],
+                [" ", " ", ";", "J", ";", " ", " ", " "],
+                [" ", " ", " ", ";", " ", " ", " ", " "]]
+ 
+grille_fin =   [[" ", " ", " ", "△", " ", " ", " ", ";"],
+                ["○", "○", "○", "○", "○", "○", "○", ";"],
+                [" ", " ", "△", "△", "△", " ", " ", ";"],
+                [" ", " ", "○", "○", "○", " ", " ", ";"],
+                ["☐", " ", "☐", " ", "☐", " ", "☐", ";"],
+                [" ", "△", "△", "△", "△", "△", " ", ";"],
+                [" ", "○", " ", "○", " ", "○", " ", ";"],
+                [" ", "☐", " ", "☐", " ", "☐", " ", ";"]]
+
+#### REPRESENTATION GRAPHIQUE
+
+def afficher_grille(grille):
+    column_header= [" ", "1", "2", "3", "4", "5", "6", "7", "8"]
+    grille.insert(0, column_header)
+    for i in range(1,9):
+        grille[i].insert(0, chr(64 + i))
+    for y in range(len(grille)):
+        b = "---------------------------------"
+        print(b)
+        a = " | ".join(grille[y])
+        print(a)
+
+afficher_grille(grille_fin)
+
+def correction_grille(grille):
+    del grille[0]
+    for i in range(0,8):
+        del grille[i][0]
+    return grille
+
+afficher_grille(grille_debut)
+
 current_position = "here"
 player_name = "Bob"
 game_map = ""
@@ -8,15 +49,17 @@ def start_game():
 def inventory():
   return 
   
-  def Menu():
+def Menu():
   # Afficher les options du menu
   # demander à l'utilisateur 
   # en fonction du choix appeler une des 4 fonctions du dessous
+  return
 
 def start_game():
   # afficher le contexte et le début de l'histoire
   # ask_name()
   # move()
+  return 
 
 def ask_name():
   # demander le nom
@@ -32,18 +75,20 @@ def credits():
 
 def exit():
   # quitter le jeu
+  return
 
 def move():
   # print les propositions de déplacement
   # recupérer le choix de l'utilisateur 
   # En fonction du choix appeler une fonctionnalité
+  return
 
 def fight():
   # présentation du combat
   # print propositions d'actions du joueur
   # en fonction du choix du joueurs appeler la fonction qui correspond
   # action de l'ennemis
-
+  return
 
 def find_object():
   return
