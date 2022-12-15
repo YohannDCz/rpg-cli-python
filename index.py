053d69a7418277b5b36c2922098ab3debd57aba6
Yohann DC <YohannDCz@gmail.com>
2022-12-15 11:04:45 +0100

    Change the denominations

--- index.py
+++ index.py
@@ -1,43 +1,44 @@
-level1 = [[";", " ", " ", " ", " ", " ", " ", " ", ";"],
-          [";", "☐", "☐", "☐", "☐", "☐", "☐", "☐", ";"],
-          [";", " ", " ", " ", "○", " ", " ", " ", ";"],
-          [";", " ", "○", "○", "○", "○", "○", " ", ";"],
-          [" ", ";", " ", "☐", "☐", "☐", " ", ";", " "],
-          [" ", " ", ";", " ", " ", " ", ";", " ", " "],
-          [" ", " ", " ", ";", "J", ";", " ", " ", " "],
-          [" ", " ", " ", " ", ";", " ", " ", " ", " "]]
+level1 = [["l ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "l "],
+          ["l ", "o2", "o2", "o2", "o2", "o2", "o2", "o2", "l "],
+          ["l ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "l "],
+          ["l ", "  ", "e1", "e1", "e1", "e1", "e1", "  ", "l "],
+          ["  ", "l ", "  ", "o1", "o1", "o1", "  ", "l ", "  "],
+          ["  ", "  ", "l ", "  ", "  ", "  ", "l ", "  ", "  "],
+          ["  ", "  ", "  ", "l ", "J ", "l ", "  ", "  ", "  "],
+          ["  ", "  ", "  ", "  ", "l ", "  ", "  ", "  ", "  "]]
  
-level2 = [[";", " ", " ", " ", " ", " ", " ", " ", ";"],
-          [";", "○", "○", "○", "○", "○", "○", "○", ";"],
-          [";", " ", " ", "△", "△", "△", " ", " ", ";"],
-          [";", " ", " ", "○", "○", "○", " ", " ", ";"],
-          [";", "☐", " ", "☐", " ", "☐", " ", "☐", ";"],
-          [";", " ", "△", "△", "△", "△", "△", " ", ";"],
-          [";", " ", "○", " ", "○", " ", "○", " ", ";"],
-          [";", " ", "☐", " ", "☐", " ", "☐", " ", ";"]]
+level2 = [["l ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "l "],
+          ["l ", "e4", "e4", "e4", "e4", "e4", "e4", "e4", "l "],
+          ["l ", "  ", "  ", "p2", "p2", "p2", "  ", "  ", "l "],
+          ["l ", "  ", "  ", "e3", "e3", "e3", "  ", "  ", "l "],
+          ["l ", "o4", "  ", "o4", "  ", "o4", "  ", "o4", "l "],
+          ["l ", "  ", "p1", "p1", "p1", "p1", "p1", "  ", "l "],
+          ["l ", "  ", "e2", "  ", "e2", "  ", "e2", "  ", "l "],
+          ["l ", "  ", "o3", "  ", "o3", "  ", "o3", "  ", "l "]]
 
-level3 = [[";", " ", " ", " ", " ", " ", " ", " ", ";"],
-          [";", " ", "P", " ", "P", " ", "P", " ", ";"],
-          [";", " ", " ", " ", " ", " ", " ", " ", ";"],
-          [";", "○", "○", "○", "○", "○", "○", "○", ";"],
-          [";", " ", " ", " ", " ", " ", " ", " ", ";"],
-          [";", "△", "△", "△", "△", "△", "△", "△", ";"],
-          [";", " ", "☐", " ", "☐", " ", "☐", " ", ";"],
-          [";", " ", " ", " ", " ", " ", " ", " ", ";"]]
+level3 = [["l ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "l "],
+          ["l ", "  ", "P ", "  ", "P ", "  ", "P ", "  ", "l "],
+          ["l ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "l "],
+          ["l ", "e5", "e5", "e5", "e5", "e5", "e5", "e5", "l "],
+          ["l ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "l "],
+          ["l ", "p3", "p3", "p3", "p3", "p3", "p3", "p3", "l "],
+          ["l ", "  ", "o5", "  ", "o5", "  ", "o5", "  ", "l "],
+          ["l ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "l "]]
+
 ### J = Joueur
 ### P = Princesse
-### ; = limite de jeu
-### ☐ = objet
-### ○ = ennemi
-### △ = potion
+### l = limite de jeu
+### o = objet
+### e = ennemi
+### p = potion
 
 def display_grid(grid):
-    column_header= [" ", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    column_header= [" ", "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 "]
     grid.insert(0, column_header)
     for i in range(1,9):
         grid[i].insert(0, chr(64 + i))
     for y in range(len(grid)):
-        b = "--------------------------------------"
+        b = "-----------------------------------------------"
         print(b)
         a = " | ".join(grid[y])
         print(a)
@@ -57,9 +58,9 @@ def inventory():
   return 
   
 def Menu():
-  # Afficher les options du menu
+  # Afficher lel optionl du menu
   # demander à l'utilisateur 
-  # en fonction du choix appeler une des 4 fonctions du dessous
+  # en fonction du choix appeler une del 4 fonctionl du dessous
   return
 
 def start_game():
@@ -73,11 +74,11 @@ def ask_name():
   return name
 
 def load_game():
-  # charger des datas en mémoire
+  # charger del datal en mémoire
   start_game()
 
 def credits():
-  # print afficher des infos sur la teams 
+  # print afficher del infol sur la teaml 
   Menu()
 
 def exit():
@@ -85,15 +86,15 @@ def exit():
   return
 
 def move():
-  # print les propositions de déplacement
+  # print lel propositionl de déplacement
   # recupérer le choix de l'utilisateur 
   # En fonction du choix appeler une fonctionnalité
   return
 
 def fight():
   # présentation du combat
-  # print propositions d'actions du joueur
-  # en fonction du choix du joueurs appeler la fonction qui correspond
+  # print propositionl d'actionl du joueur
+  # en fonction du choix du joueurl appeler la fonction qui correspond
   # action de l'ennemis
   return
 
